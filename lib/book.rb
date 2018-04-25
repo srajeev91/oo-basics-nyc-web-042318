77c03baaf69f728d24e47139f4e696984c810ccb
srajeev91 <github email address>
2018-04-25 10:08:26 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,29 +1,46 @@
 class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
 
   def initialize(title)
     @title = title
   end
 
-  def title
-    @title
-  end
-
-  attr_accessor :author
-    ##The above is the short form for the comment
-    # def author=(author)
-    #   @author = author
-    # end
-    #
-    # def author
-      # @author
-    # end
-
-  attr_accessor :page_count
-
-  attr_accessor :genre
-
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-
 end
+
+
+
+
+
+
+
+#   def initialize(title)
+#     @title = title
+#   end
+#
+#   def title
+#     @title
+#   end
+#
+#   attr_accessor :author
+#     ##The above is the short form for the comment
+#     # def author=(author)
+#     #   @author = author
+#     # end
+#     #
+#     # def author
+#       # @author
+#     # end
+#
+#   attr_accessor :page_count
+#
+#   attr_accessor :genre
+#
+#   def turn_page
+#     puts "Flipping the page...wow, you read fast!"
+#   end
+#
+# end
